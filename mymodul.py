b9f8bcb62ffbb7b04b5d5a938a2a091161a46897
ANANDKUMAR <ak6491553@gmail.com>
2025-02-27 16:18:25 +0530

    Initial commit

---
+++ mymodul.py
@@ -0,0 +1,14 @@
+def greeting(name):
+    print('good morning',name)
+  
+    
+def anand(names):
+    print("kaise ho aap sir ji,",names)
+  
+def kumar(anandakumar):
+    print('tera nam ananDKumar hai',anandakumar)
+ 
+def sarvik(Sarvik):
+    print('bhai kya hal chal hai sabh thik hai n ?',Sarvik )
+    
+    
